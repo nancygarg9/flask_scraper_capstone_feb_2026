3ef1d0b678d4c8dc245f6df0f5d41c580847abf3
nancygarg <Example@google.com>
2026-03-17 22:23:29 -0500

    updated

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # Flask Dashboard setup
 
 from flask import Flask,render_template
-from scraper import scrape_quotes, quotes_to_df
+from scraper import scrape_quotes, quotes_to_df,scrape_books, books_to_df
 
 app = Flask(__name__) # tells FLASK to create the app - object - so it knows where to find the templates, static files,etc.
 
@@ -11,25 +11,37 @@ def hello():
 
 @app.route("/")
 def dashboard():
-    quotes = scrape_quotes(limit=4) 
+    quotes = scrape_quotes(limit=10) 
     df =quotes_to_df(quotes)
     #return "Hello World!"
-    return render_template("dashboard.html",tables=[df.to_html (classes='data')],titles=df.columns.values)
+    return render_template("dashboard.html",tables=[df.to_html (classes='data')],titles=df.columns.values, page_title="Quotes Dashboard", page_type="quotes")
     # converts the Dataframe DF into an html table
     # jinja2 -> flask template engine. it is what we use to work with python code inside the html
     # enhacenemnts - add more decorators like /about and put your description
 
+<<<<<<< HEAD
 # Books route added as enhancement by ***NANCY GARG***
+=======
+
+
+>>>>>>> e697ec0 (updated)
 @app.route("/books")
 def dashboard_books():
     books = scrape_books(limit=10) 
     df =books_to_df(books)
     #return "Hello World!"
     return render_template("dashboard.html",tables=[df.to_html (classes='data')],titles=df.columns.values, page_title="Books Dashboard", page_type="books")
+<<<<<<< HEAD
 
+=======
+    # converts the Dataframe DF into an html table
+    # jinja2 -> flask template engine. it is what we use to work with python code inside the html
+    # enhacenemnts - add more decorators like /about and put your description
+>>>>>>> e697ec0 (updated)
 """ @app.route("/about")
 def about():
     # put your code here """
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=5500, debug=False) # use 5000 as port in your deployment unless you have anything running on that port.
--- scraper.py
+++ scraper.py
@@ -12,6 +12,7 @@ logging.basicConfig(level=logging.DEBUG)
 
 def scrape_quotes(limit=5):  # default arg
     url = "https://quotes.toscrape.com/"
+<<<<<<< HEAD
 
     response = requests.get(url)  # fetches webpage.
     if response.status_code != 200:  # 200 is for successful fetch
@@ -41,22 +42,80 @@ def scrape_books(limit=5):
 
     return books[:limit]
 
+=======
+   
+    response = requests.get(url) # fetches webpage. 
+    if response.status_code!=200: # 200 is for successful fetch, and you can later enhance it by using multiple try and except block here. for eg if you get 404 - you can catch the exception and print a better message like Page not found.
+        logging.error("Failed to fetch the website and it returned code as : ", response.status_code)
+        return [] # store them in a list but here since we didnt get the webage we return an empty list
+
+   
+    
+    
+    soup = BeautifulSoup(response.text,"html.parser") # parses the html response that we received above from that url.
+    #print(soup.prettify())
+    #<span class="text" itemprop="text">“The world as we have created it is a process of our thinking. It cannot be changed without changing our thinking.”</span>
+    quotes = [q.text  for q in soup.find_all("span", class_="text")]  #list comprehension
+    #books  = [b.text  for b in soup.find_all("span", class_="text")] Nancy
+    # quotes will be list of all the quotes - just the text that we wanted.
+    #return quotes # list of ALL the quotes but i shud return only the limit
+    return quotes[:limit] # 0-4 get the first five quotes, start=0, stop =limi-1,
+>>>>>>> e697ec0 (updated)
+
+
+def scrape_books(limit=5):
+    url="https://books.toscrape.com/" # enhance using this for books and author
+    response = requests.get(url) # fetches webpage. 
+    if response.status_code!=200: # 200 is for successful fetch, and you can later enhance it by using multiple try and except block here. for eg if you get 404 - you can catch the exception and print a better message like Page not found.
+        logging.error("Failed to fetch the website and it returned code as : ", response.status_code)
+        return [] # store them in a list but here since we didnt get the webage we return an empty list
+
+   
+    
+    
+    soup = BeautifulSoup(response.text,"html.parser") # parses the html response that we received above from that url.
+    #print(soup.prettify())
+    #<span class="text" itemprop="text">“The world as we have created it is a process of our thinking. It cannot be changed without changing our thinking.”</span>
+    #<a href="../../../full-moon-over-noahs-ark-an-odyssey-to-mount-ararat-and-beyond_811/index.html" title="Full Moon over Noah’s Ark: An Odyssey to Mount Ararat and Beyond">Full Moon over Noah’s ...</a>
+    books = [t.h3.a["title"]  for t in soup.find_all("article", class_="product_pod")]  #list comprehension
+    #books  = [b.text  for b in soup.find_all("span", class_="text")] Nancy
+    # quotes will be list of all the quotes - just the text that we wanted.
+    #return quotes # list of ALL the quotes but i shud return only the limit
+    return books[:limit] # 0-4 get the first five quotes, start=0, stop =limi-1,
+
+
 
 # Pandas Dataframe - very handy for tabular form of representation
 def quotes_to_df(quotes):
     df = pd.DataFrame(quotes, columns=["Quotes"])
     return df
 
+<<<<<<< HEAD
 
 # books function added by ***NANCY GARG***
 
 def books_to_df(books):
     df = pd.DataFrame(books, columns=["Books"])
+=======
+    df = pd.DataFrame(quotes,columns=["Quotes"])
+    return df
+
+def books_to_df(books):  # Nancy
+
+    df = pd.DataFrame(books,columns=["Books"])  # Nancy
+    # convert our list of quotes into pandas timeframe that will look something like this:
+>>>>>>> e697ec0 (updated)
 
     '''
         Quote
     0   <actual quote>“The world as we have created it is a process of our thinking. It cannot be changed without changing our thinking.”
     1   “It is our choices, Harry, that show what we truly are, far more than our abilities.”
     '''
-
     return df
+<<<<<<< HEAD
+=======
+
+
+
+
+>>>>>>> e697ec0 (updated)
