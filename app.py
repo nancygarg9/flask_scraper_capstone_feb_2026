69c36143f15027330f478655bcee328825cd810c
nancygarg <Example@google.com>
2026-03-19 10:13:49 -0500

    import missing functions

--- app.py
+++ app.py
@@ -19,25 +19,16 @@ def dashboard():
     # jinja2 -> flask template engine. it is what we use to work with python code inside the html
     # enhacenemnts - add more decorators like /about and put your description
 
-<<<<<<< HEAD
-# Books route added as enhancement by ***NANCY GARG***
-=======
 
 
->>>>>>> e697ec0 (updated)
 @app.route("/books")
 def dashboard_books():
     books = scrape_books(limit=10) 
     df =books_to_df(books)
     #return "Hello World!"
     return render_template("dashboard.html",tables=[df.to_html (classes='data')],titles=df.columns.values, page_title="Books Dashboard", page_type="books")
-<<<<<<< HEAD
 
-=======
     # converts the Dataframe DF into an html table
-    # jinja2 -> flask template engine. it is what we use to work with python code inside the html
-    # enhacenemnts - add more decorators like /about and put your description
->>>>>>> e697ec0 (updated)
 """ @app.route("/about")
 def about():
     # put your code here """
--- test_scraper.py
+++ test_scraper.py
@@ -1,5 +1,6 @@
 # rule of thumb is test every function and class/object that you have written inside your python module(s)
 from scraper import scrape_quotes,quotes_to_df,scrape_books,books_to_df
+from scraper import scrape_quotes,quotes_to_df,scrape_books,books_to_df
 
 
 def test_scrape_quotes():
@@ -10,9 +11,14 @@ def test_scrape_books():
     books = scrape_books(limit=3)
     assert len(books) == 3 # getting back 3 entries in my list or not. 
 
+def test_scrape_books():
+    books = scrape_books(limit=3)
+    assert len(books) == 3 # getting back 3 entries in my list or not. 
+
 def test_quotes_to_df():
     df = quotes_to_df(["ThinkPython", "AI"]) # treat this as quotes and we are passing only 2 elements "ThinkPython" and "AI"
     assert df.shape[0] ==  2
+    assert "Quotes" in df.columns
     assert "Quotes" in df.columns
     # enhancement woudl be add a third column name as "Author" and assert that as well.
 
