e12ffb13b5c64da85f8aae73bc521c078e35d642
nancygarg <Example@google.com>
2026-03-19 23:47:51 -0500

    fixed pytest

--- test_scraper.py
+++ test_scraper.py
@@ -4,22 +4,21 @@ from scraper import scrape_quotes,quotes_to_df,scrape_books,books_to_df
 
 
 def test_scrape_quotes():
-    quotes = scrape_quotes(limit=3)
+    quotes,author = scrape_quotes(limit=3)
     assert len(quotes) == 3 # getting back 3 entries in my list or not. 
+    assert len(author) == 3
 
 def test_scrape_books():
     books = scrape_books(limit=3)
     assert len(books) == 3 # getting back 3 entries in my list or not. 
 
-def test_scrape_books():
-    books = scrape_books(limit=3)
-    assert len(books) == 3 # getting back 3 entries in my list or not. 
+
 
 def test_quotes_to_df():
-    df = quotes_to_df(["ThinkPython", "AI"]) # treat this as quotes and we are passing only 2 elements "ThinkPython" and "AI"
+    df = quotes_to_df(["ThinkPython", "AI"],["Nancy Garg", "Mickey Mouse"]) # treat this as quotes and we are passing only 2 elements "ThinkPython" and "AI"
     assert df.shape[0] ==  2
     assert "Quotes" in df.columns
-    assert "Quotes" in df.columns
+    assert "Author" in df.columns
     # enhancement woudl be add a third column name as "Author" and assert that as well.
 
 def test_books_to_df():
